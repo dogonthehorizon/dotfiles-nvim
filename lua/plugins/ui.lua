1481d782b070b8f008173035765c73b053eef80a
Fernando Freire <dogonthehorizon@gmail.com>
2025-01-11 10:04:00 -0800

    chore: update telescope bindings

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -30,7 +30,7 @@ return {
       { "<leader>sn", "", desc = "+noice"},
       { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
       { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-      { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
+      { "<leader>snh", function() require("noice").cmd("pick") end, desc = "Noice History" },
       { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
       { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
       { "<leader>snt", function() require("noice").cmd("pick") end, desc = "Noice Picker (Telescope/FzfLua)" },
